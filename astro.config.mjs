4b46f9d942c0f461904e08d3409670a4ed31ba74
arvind singharpuria <arvind.020606@gmail.com>
2024-03-30 23:33:33 +0530

    fix: post.render error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
-import vercel from '@astrojs/vercel/serverless';
+//import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 
 // https://astro.build/config
@@ -17,7 +18,8 @@ export default defineConfig({
       wrap: false
     }
   },
-  output: 'server',
+  //output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
