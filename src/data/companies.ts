1e2fbd67e34a0ac6a93cad2234f228f99836589e
arvind singharpuria <arvind.020606@gmail.com>
2025-09-09 22:52:32 +0530

    feat: updated companies logos and testimonial section

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -1,6 +1,7 @@
 type Company = {
   name: string;
   logo: string;
+  darkLogo?: string;
   website?: string;
   description?: string;
 };
@@ -8,7 +9,7 @@ type Company = {
 const companies: Company[] = [
   {
     name: "Fountane",
-    logo: "/img/companies/fountane-logo.png",
+    logo: "/img/companies/fountane.png",
     website: "https://www.fountane.com/",
     description: "Software Engineer"
   },
@@ -30,18 +31,47 @@ const companies: Company[] = [
     website: "https://www.rechat.vc/",
     description: "React Developer"
   },
-//   {
-//     name: "Netflix",
-//     logo: "/img/companies/netflix.svg",
-//     website: "https://netflix.com",
-//     description: "UI/UX Developer"
-//   },
-//   {
-//     name: "Apple",
-//     logo: "/img/companies/apple.svg",
-//     website: "https://apple.com",
-//     description: "iOS Developer"
-//   }
+  {
+    name: "OpenSSF",
+    logo: "/img/companies/openssf.avif",
+    darkLogo: "/img/companies/openssf-light.svg",
+    website: "https://openssf.org/",
+    description: "Open Source Developer"
+  },
+  {
+    name: "Cd Foundation",
+    logo: "/img/companies/cdf.svg",
+    darkLogo: "/img/companies/cdf-white.svg",
+    website: "https://cd.foundation/",
+    description: "Open Source Developer"
+  },
+  {
+    name: "Linux Foundation",
+    logo: "/img/companies/linux-foundation.png",
+    darkLogo: "/img/companies/linux-foundation-light.png",
+    website: "https://www.linuxfoundation.org/",
+    description: "Open Source Developer"
+  },
+  {
+    name: "ComplyIQ",
+    logo: "/img/companies/complyiq.webp",
+    darkLogo: "/img/companies/complyiq-light.png",
+    website: "https://www.complyiq.io/",
+    description: "DevOps Engineer"
+  },
+  {
+    name: "ASI",
+    logo: "/img/companies/asi.png",
+    website: "https://anandsystems.com/",
+    description: "Automation Engineer"
+  },
+  {
+    name: "PMC",
+    logo: "/img/companies/pmc.jpg",
+    darkLogo: "/img/companies/pmc.webp",
+    website: "https://parkingmgt.com/",
+    description: "Automation Engineer"
+  }
 ];
 
 export default companies;
