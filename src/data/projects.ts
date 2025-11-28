8242a96b56c6a5173ca22d9ffbd21ab61b91d672
arvind singharpuria <arvind.020606@gmail.com>
2025-11-28 17:02:27 +0530

    feat: add new projects and images for Shotly AI and Evol Jewels

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,6 +10,14 @@ export type Project = {
 
 const projects: Project[] = [
     {
+    title: "AI SaaS - Shotly AI (300+ users)",
+    techs: ["Next JS", "Tailwind CSS", "Supabase", "Drizzle", "fal AI"],
+    link: "https://shotlyai.com/",
+    description: "A SaaS platform to turn your selfie into a professional photoshoot, in seconds.",
+    screenshot: "/img/projects/shotly-landing-page.png",
+    hoverText: "AI Headshot Generator",
+  },
+  {
     title: "AI SaaS - Rechat",
     techs: ["Next JS", "Tailwind CSS", "Shadcn UI", "Clerk", "Prisma", "Postgresql", "OpenAI"],
     link: "https://rechat.vc/",
@@ -24,6 +32,14 @@ const projects: Project[] = [
     description: "Full-stack platform for generating professional profile pictures with credit system and payment integration.",
     screenshot: "/img/projects/profilkep-landing-page.png",
     hoverText: "Full-stack platform for generating professional profile pictures",
+  },
+  {
+    title: "AI Jewelry Stylist Kiosk (Won $500 USD and 2nd place in Hackathon)",
+    techs: ["Next JS", "Tailwind CSS", "PostgreSQL", "Prisma", "NodeMailer"],
+    link: "https://hackathon-beryl-iota.vercel.app/",
+    description: "An AI-powered jewelry recommendation system that analyzes user photos and generates personalized jewelry suggestions using advanced computer vision and celebrity style matching.",
+    screenshot: "/img/projects/evol-jewels.png",
+    hoverText: " Responsive virtual try-on web app for Evol Jewels that lets users try jewelry using only sliders and clicks.",
   },
   {
     title: "Ortelius (Open Source)",
